c4d2151cc09e64a8d05656d1a12182958ef519e1
shlomo <shlomo@dev.com>
2024-11-13 17:44:26 +0200

    connection the redux and the socket in the main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import './index.css'
 import App from './App.tsx'
+import { io } from "socket.io-client";
+import "dotenv/config";
+
+export const socket = io(`${process.env.API_URL}`);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
